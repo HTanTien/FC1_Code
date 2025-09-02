97d36990a7996e8eb599b8213d0dea77500368c3
Huỳnh Tấn Tiến <189233680+HTanTien@users.noreply.github.com>
2025-09-02 20:12:18 +0700

    Add files via upload

---
+++ LEC6_ARRAY_ARRAYLISTEx/Ex13_MergeTwoSortedArrays.java
@@ -0,0 +1,9 @@
+package LEC6_ARRAY_ARRAYLISTEx;
+import java.util.*;
+public class Ex13_MergeTwoSortedArrays {
+    static int[] merge(int[] a, int[] b) {
+        int[] r = new int[a.length + b.length];
+        int i=0,j=0,k=0;
+        
+    }
+}
